cdf17086a3c71a792ff6626cc4b5d50adead5166
shujinarazaki <shujinarazaki@protonmail.com>
2018-07-20 17:50:13 +0900

    That almost works.

--- app/Main.hs
+++ app/Main.hs
@@ -3,33 +3,52 @@ module Main where
 --import Lib
 import Graphics.Gloss
 import Prelude                                  hiding ( lines )
-import Control.Concurrent (forkIO, killThread, myThreadId, threadDelay
-                          , newEmptyMVar, putMVar, readMVar)
+import Control.Concurrent (forkIO, killThread, myThreadId, threadDelay, MVar(..)
+                          , newEmptyMVar, takeMVar)
 import Control.Exception
 import Control.Monad ((<=<), unless, void, when)
 import Data.IORef
+import System.Environment
 import System.IO.Unsafe
 import System.Random
+import SAT.Mios
 
 getRand :: Float -> Float
 getRand _ = unsafePerformIO $ getStdRandom (randomR (30, 100))
 
+dump :: MVar [Int]
+dump = unsafePerformIO newEmptyMVar
+
+getSolverData :: Float -> [Int]
+getSolverData _ = unsafePerformIO $ takeMVar dump
+
 xp :: Float -> IORef Float
 xp t = unsafePerformIO $ newIORef 20
 
---main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
 main :: IO ()
 main = do
-  void $ forkIO $ animate (InWindow "Clock" (600, 600) (20, 20)) black frame
-  void $ getLine
+  (f:_) <- (++ ["/home/narazaki/Repositories/SATbench/sudoku/sudoku16.cnf"]) <$> getArgs
+  void $ forkIO $ animate (InWindow f (800, 500) (20, 20)) black frame
+  executeSolverSlicedOn dump f
+  threadDelay 12000000
   return ()
 
 frame :: Float -> Picture
-frame   time
-        = Color white
-        $ Scale (getRand time) (getRand time) -- 120
-        $ Rotate (time * 2*pi)
-        $ clockFractal 5 time
+frame time = Color white $ Scale 1.8 1.8 $ Translate 0 (-70) $ makeGrid (getSolverData time)
+
+num2pos :: Int -> (Float, Float)
+num2pos n = (fromIntegral (mod n 100) * 4 - 200, 150 - fromIntegral (div n 100) * 4 - 20)
+
+makeGrid :: [Int] -> Picture
+makeGrid l = Pictures $ title : zipWith light l [1 .. ]
+  where
+    title = Translate (-200) 160 $ Scale 0.07 0.07 $ Text "searching..."
+    light :: Int -> Int -> Picture
+    light state n
+      | state == -1 = Color blue        $ Translate x y $ circleSolid 1.7
+      | state ==  0 = color (greyN 0.2) $ Translate x y $ circleSolid 1.4
+      | state ==  1 = Color white       $ Translate x y $ circleSolid 1.7
+      where (x, y) = num2pos n
 
 clockFractal :: Int -> Float -> Picture
 clockFractal 0 _        = Blank
